c99985534e5ffe44752f1f8e84a3b381fb246e6f
Kwang Bin Lee <klee166@jhu.edu>
2018-05-18 04:34:18 -0400

    Clustering for Hiearchy and K mean

--- similarity.py
+++ similarity.py
@@ -3,8 +3,12 @@ import numpy as np
 import nltk
 from nltk.tokenize import word_tokenize
 from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.cluster import KMeans
+from sklearn.cluster import AgglomerativeClustering
+import matplotlib.pyplot as plt
 import string
 
+# Coming up with stemmer
 stemmer = nltk.stem.porter.PorterStemmer()
 remove_punctuation_map = dict((ord(char), None) for char in string.punctuation)
 printable = set(string.printable)
@@ -23,6 +27,7 @@ def normalize(txt):
 docs_vector = {}
 gen_docs = []
 raw_documents = []
+list_names = []
 name = ""
 temp_txt = ""
 first = -1
@@ -33,6 +38,9 @@ with open('myfile.txt','rb') as f:
 		text = text.decode('utf-8')
 		temp_temp_txt = text.rstrip()
 		if(".I" == temp_temp_txt[0:2]):
+			temp_temp_temp_txt = temp_temp_txt.split(" ")
+			name = temp_temp_temp_txt[1]
+			list_names.append(name)
 			if(first == 1):
 				#print temp_txt
 				raw_documents.append(temp_txt)
@@ -45,8 +53,38 @@ with open('myfile.txt','rb') as f:
 	#print raw_documents
 	vect = TfidfVectorizer(tokenizer=normalize, stop_words='english').fit_transform(raw_documents)
 	pairwise_similarity = ((vect * vect.T).A)
+
+	# predict k_means
+	eigen_values, eigen_vectors = np.linalg.eigh(pairwise_similarity)
+	kmeans = KMeans(n_clusters=5, init='k-means++').fit_predict(pairwise_similarity)
+	kmeans_f = KMeans(n_clusters=5, init='k-means++').fit(pairwise_similarity)
+
+
+
+	hcluster = AgglomerativeClustering(n_clusters=5).fit_predict(pairwise_similarity)
+	hcluster_f = AgglomerativeClustering(n_clusters=5).fit(pairwise_similarity)
+
+	# categorize and come up with k means
+	print ("K-Mean Clustering")
+	j = 0
+	for i in np.nditer(kmeans):
+		print list_names[j]
+		print i
+		j = j + 1
+
+	j = 0
+	print("Hierachy Clustering")
+	for i in np.nditer(hcluster):
+		print list_names[j]
+		print i
+		j = j + 1
+
+
+
+	plt.xlabel('X')
+	plt.ylabel('Y')
+	plt.scatter(pairwise_similarity[:,0], pairwise_similarity[:,1],
+		c=kmeans_f.labels_, edgecolor='')
+	plt.show()
 	np.set_printoptions(threshold=np.nan)
 f.close()
-
-#sims = gensim.similarities.Similarity()
-#print(corpus)
